cd16f7f6bfc21c9184424e716880476beba88373
maxwhitehouse <146250785+maxwhitehouse@users.noreply.github.com>
2026-02-25 10:49:53 -0800

    Add files via upload

---
+++ OP_1/js/scripts.js
@@ -0,0 +1,48 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'jpg'
+}).addTo(map);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//     .openPopup();
+
+
+// var myDiv = document.getElementById("example");
+// myDiv.innerHTML = "Look at this"
+
+fetch('/data/airports.geojson')
+  .then(response => response.json())
+  .then(data => {
+    var vorP = turf.voronoi(data) 
+    var Kport = turf.point([ -97.428738768397594, 37.652927960390301 ])
+    L.geoJson(vorP, {
+      onEachFeature: function (feature, layer) {
+        layer.on('click', function () {
+          var props = feature.properties;
+
+          layer.bindPopup(
+            "<b>" + props.name + "</b><br>"
+          ).openPopup();
+
+        });
+
+      }
+    }).addTo(map);
+
+  })
+  .catch(err => console.error(err));
+
+  // for loop
+  // distance from every point 
+
+// var from = turf.point([-75.343, 39.984]);
+// var to = turf.point([-75.534, 39.123]);
+// var options = { units: "miles" };
+
+// var distance = turf.distance(from, to, options);
+
