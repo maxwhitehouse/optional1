a84da2e859a709053928e1a1f72787652bdbdda0
maxwhitehouse <146250785+maxwhitehouse@users.noreply.github.com>
2026-03-02 10:00:28 -0800

    Add files via upload

--- OP_1/js/scripts.js
+++ OP_1/js/scripts.js
@@ -1,8 +1,8 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(map);
 
 // L.marker([51.5, -0.09]).addTo(map)
@@ -43,6 +43,4 @@ fetch('/data/airports.geojson')
 // var options = { units: "miles" };
 
 // var distance = turf.distance(from, to, options);
-
-
 
