f9087a16aa0578c2be660097ace54af975e301df
maxwhitehouse <146250785+maxwhitehouse@users.noreply.github.com>
2026-03-02 09:51:27 -0800

    test scripts.js

--- OP_1/js/scripts.js
+++ OP_1/js/scripts.js
@@ -1,10 +1,8 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'jpg'
+var L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // L.marker([51.5, -0.09]).addTo(map)
@@ -45,4 +43,5 @@ fetch('/data/airports.geojson')
 // var options = { units: "miles" };
 
 // var distance = turf.distance(from, to, options);
+
 
